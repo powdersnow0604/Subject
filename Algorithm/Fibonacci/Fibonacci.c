baf98acccb1b98879a6c6043ca9936cbb386c508
powdersnow0604 <gundam6324@naver.com>
2023-10-01 18:39:02 +0900

    Update Fibonacci.c

--- Algorithm/Fibonacci/Fibonacci.c
+++ Algorithm/Fibonacci/Fibonacci.c
@@ -18,19 +18,21 @@ size_t k_n(size_t k, size_t n)
 
 long long fibonacci(int n)
 {
+	switch (n)
+	{
+	case 0: return 0;
+	case 1:
+	case 2: return 1;
+	}
+
 	int i;
 	size_t f[3] = { 0,1,1 };
-	size_t x[3] = { 1,0,1 };
+	size_t x[3];
 	size_t temp;
 
-	if ((n - 3) & 1)
-	{
-		temp = x[0] * f[1] + x[1] * f[2];
-		x[1] = x[1] * f[1];
-		x[0] = x[0] * f[0] + x[1];
-		x[2] = x[2] * f[2] + x[1];
-		x[1] = temp;
-	}
+	if ((n - 3) & 1) { x[0] = 0; x[1] = 1; x[2] = 1; }
+	else { x[0] = 1; x[1] = 0; x[2] = 1; }
+	
 
 	for (i = 2; i <= n - 3; i <<= 1) {
 		//start
